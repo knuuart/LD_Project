f9d2b407bd7e96814e148ac072ff664fb49c383f
OPISTO\ville.varis <ville.vm.varis@gmail.com>
2017-04-22 13:23:40 +0300

    Tehty liikeskriptit pelaajalle ja vihollisten waypointeille testiskeneen

---
+++ LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed;
+    public Transform[] waypoints;
+    public float tolerance = 1f;
+    int targetIndex = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveToPoint();
+		
+	}
+
+
+    void MoveToPoint()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[targetIndex].position, Time.deltaTime * speed);
+
+        Vector3 direction;
+        direction = waypoints[targetIndex].position - transform.position;
+
+       if(direction.magnitude < tolerance)
+        {
+            targetIndex += 1;
+            if(targetIndex == waypoints.Length)
+            {
+                targetIndex = 0;
+            }
+        }
+    }
+}
---
+++ LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Vector3 pos;
+    public float speed;
+
+
+    private void Start()
+    {
+        Vector3 pos = transform.position;
+    }
+
+
+	
+	void Update () {
+    /*
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        MovePlayer(new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime);
+        */
+
+        if(Input.GetKey(KeyCode.A) && transform.position == pos)
+        {
+            pos += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position == pos)
+        {
+            pos += Vector3.right;
+        }
+
+        if (Input.GetKey(KeyCode.S) && transform.position == pos)
+        {
+            pos += Vector3.down;
+        }
+
+        if (Input.GetKey(KeyCode.W) && transform.position == pos)
+        {
+            pos += Vector3.up;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+
+
+
+    }
+    /*
+    void MovePlayer (Vector3 dir)
+    {
+        transform.position += dir;
+    }
+    */
+
+}
