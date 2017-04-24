fbd1cd80be78353ccacb0f4a7ad8f99a7105b02e
OPISTO\ville.varis <ville.vm.varis@gmail.com>
2017-04-24 18:31:33 +0300

    no message

--- LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/SpriteRendererOrder.cs
+++ LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/SpriteRendererOrder.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteRendererORder : MonoBehaviour {
+public class SpriteRendererOrder : MonoBehaviour {
+
+    SpriteRenderer sr;
 
 	// Use this for initialization
 	void Start () {
-		
+        sr = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        sr.sortingOrder = (int) -transform.position.y *3;
 		
 	}
 }
