d274eb1c53fea016c1e63b79f38878d4df642f11
knuuart <arttu.knuutinen@gmail.com>
2017-04-24 22:44:38 +0300

    Shame resettaa jos pelin restarttaa

--- LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/CameraClamper.cs
+++ LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/CameraClamper.cs
@@ -6,7 +6,6 @@ public class CameraClamper : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
--- LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/GameOver.cs
+++ LD_Stealthpeli/LD_Stealthpeli/Assets/Scripts/GameOver.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour {
+	GameManagerScript gm;
 
 	// Use this for initialization
 	void Start () {
-		
+		gm = GetComponent<GameManagerScript> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameManagerScript.ShameMeter = 0;
 			SceneManager.LoadScene ("alkuruutu");
+			
 		}
 	}
 }
